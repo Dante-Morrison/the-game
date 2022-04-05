ae50406fc8611e6ffbf130955142352f3ecac10a
Dante-Morrison <37248802+Dante-Morrison@users.noreply.github.com>
2022-04-05 16:52:14 -0600

    uploaded main game files

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    // Update is called once per frame
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster() 
+    {
+        Debug.Log("Successfully Connected To Server");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 01", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer) {
+        {
+            Debug.Log(newPlayer + " has joined the room");
+            base.OnPlayerEnteredRoom(newPlayer);
+        }
+    }
+}
